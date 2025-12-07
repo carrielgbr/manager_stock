9ef10f0c245814dd4deba3e54b91ad29754fb9e2
carriel <carrielgbr@gmail.com>
2025-12-07 00:04:49 -0300

    add. new files

---
+++ users_screen/users_screen.py
@@ -0,0 +1,13 @@
+class user_screen:
+    
+    def screen_print(self):
+        print("Pesquisar Usuário")
+        print("Cadastrar Usuário")
+
+    def search_user(self):
+        user_name = input("Digite o nome do usuário que deseja pesquisar: ")
+        print(f"Pesquisando usuário: {user_name}")
+
+    def register_user(self):   
+        new_user_name = input("Digite o nome do novo usuário: ")
+        print(f"Usuário {new_user_name} cadastrado com sucesso!")   
