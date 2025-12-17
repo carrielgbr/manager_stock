1ea0bebdef287962bbe9a7dde6c523ed2bd1a864
carriel <carrielgbr@gmail.com>
2025-12-16 21:19:00 -0300

    atualizado classe estoque funcionando faltando finalizar alguns processos de estoque e produto

--- home_class/home_screen.py
+++ home_class/home_screen.py
@@ -1,8 +1,11 @@
 from home_class.home_user.home_user import home_user_teste 
 from home_estoque.screen_estoque import screen_estoque
+from home_produto.screen_produto import screen_produto
 import time
 
 estoque_screen = screen_estoque()
+produto_screen = screen_produto()
+
 
 class Home_screen:
 
@@ -34,7 +37,8 @@ class Home_screen:
                 user_screen = screen_estoque()
                 user_screen.tela_estoque()
             elif enter_user == 'produto':
-                print(enter_user, 'screen produto')
+                user_screen = screen_produto()
+                user_screen.tela_produto()
             elif enter_user == 'voltar':
                 return
             elif enter_user == 'sair':
---
+++ home_produto/screen_produto.py
@@ -0,0 +1,5 @@
+class screen_produto:
+    def tela_produto(self):
+        print("=" * 40)
+        print("TELA DE GERENCIAMENTO DE PRODUTOS")
+        print("=" * 40)
