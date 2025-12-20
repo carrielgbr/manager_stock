774b607740a8af4cd9018cd0198f7fb6f9364372
Allan Ferreira <afsn01@protonmail.com>
2025-12-20 14:42:12 -0300

    Editar usuario

--- home_class/home_user/home_user.py
+++ home_class/home_user/home_user.py
@@ -1,4 +1,6 @@
+
 import db
+import variaveis_global
 from login_cadastro_class.login_cadastro import Cadastro_logic
 
 cadastro_login = Cadastro_logic()
@@ -42,24 +44,80 @@ class home_user_teste:
         else:
             print("Usuários cadastrados:")
             for index, usuario in enumerate(db.usuarios):
-                print(f"{index}-\t{usuario['email']}")  # Mostra somente o email
+                print(f"{index} -\t{usuario['usuario']}")  # Mostra somente o usuario
         # Opção para voltar ao menu
         input("\nPressione Enter para voltar ao menu...")
 
     def editar_usuario(self):
+
+        usrs_class = Cadastro_logic()
+        selecionado = -1
+        nome_de_usuario = ""
+
         print("\n--- Editar Usuário ---")
         if not db.usuarios:
             print("Nenhum usuário cadastrado para editar.")
             return
         if db.usuarios :  # Verifica se há usuários cadastrados
-                print("Usuários cadastrados:")
+            while True:
+                print()
+                print("Usuarios:")
                 for index, usuario in enumerate(db.usuarios):
-                    print(f"{index}-\t{usuario['email']}")
-        
-        #nome = input("Nome do usuário a editar: ")
-        #print(f"Editando {nome}...")
-        #nome = input("Insira novo email: ")
-        #print(f"Email atualizado para {nome} com sucesso!")
+                    print(f"{index} -\t{usuario['usuario']}")
+                try:
+                    selecionado = input("Selecione o Index do usuario para editar: ")
+                    if selecionado == variaveis_global.voltar_flair:
+                        return
+                    if selecionado.isalpha():
+                        selecionado_temp = selecionado
+                        selecionado = int(usrs_class.usuario_existe(selecionado))
+                        if selecionado != -1:
+                            break
+                        else:
+                            print(f"Não foi encontrado o usuario \'{selecionado_temp}\'.")
+                    elif selecionado.isnumeric():
+                        selecionado = int(selecionado)
+                        if (selecionado >= 0) and (selecionado < len(db.usuarios)):
+                            break
+                        else:
+                            print(f"Selecione um index entre '0' até \'{len(db.usuarios)}\'.")
+                except:
+                    print("Digite o \'index\' ou o \'nome de usuario\' para editar.")
+
+            nome_de_usuario = db.usuarios[selecionado]["usuario"]
+            while True:
+                print()
+                print(f"Usuario \'{nome_de_usuario}\' selecionado.")
+                print("0 - Editar usuário")
+                print("1 - Editar senha")
+                print("2 - voltar")
+                acao = input("Selecione o Index: ")
+                if acao in ["2", variaveis_global.voltar_flair]:
+                    return
+                elif acao == "0":
+                    novo_nome_de_usuario = input(f"Novo nome de usuário para \'{nome_de_usuario}\': ")
+                    novo_nome_de_usuario.strip()
+                    if usrs_class.usuario_existe(novo_nome_de_usuario) == -1:
+                        if not usrs_class.editar_nome_de_usuario(selecionado, novo_nome_de_usuario):
+                            print(f"Não foi possível editar o nome de usuario do \'{nome_de_usuario}\'.")
+                        else:
+                            print(f"Sucesso: usuario \'{nome_de_usuario}\' foi alterado para \'{novo_nome_de_usuario}\'.")
+                            nome_de_usuario = novo_nome_de_usuario
+                    else:
+                        print(f"O usuário \'{novo_nome_de_usuario}\' já existe.")
+                    continue
+                elif acao == "1":
+                    auth_senha = input(f"Digite a senha atual: ")
+                    if usrs_class.usuario_auth(nome_de_usuario, auth_senha) == -1:
+                        print("Senha incorreta.")
+                        continue
+                    nova_senha = input(f"Digite a nova senha: ")
+                    if not usrs_class.editar_senha_de_usuario(selecionado, auth_senha, nova_senha):
+                        continue
+                    print(f"Sucesso: a senha do usuário \'{nome_de_usuario}\' foi alterado.")
+                else:
+                    continue
+
 
     def deletar_usuario(self):
         print("\n--- Deletar Usuário ---")
--- login_cadastro_class/login_cadastro.py
+++ login_cadastro_class/login_cadastro.py
@@ -11,6 +11,30 @@ class Cadastro_logic:
         print("*" * 40)
         print()
 
+    def nome_de_usuario_check(self, nome_de_usuario: str):
+
+        if nome_de_usuario.isnumeric():
+            print("Erro: Usuario não deve conter apenas números.")
+            return False
+        elif nome_de_usuario.isspace():
+            print("Erro: Usuario não deve conter apenas espaços.")
+            return False
+        elif not ((len(nome_de_usuario) > 3) and (len(nome_de_usuario) <= 64)):
+            print("Erro: Usuário deve conter, pelo menos, entre 4 à 64 caracteres")
+            return False
+        return True
+
+    def senha_check(self, senha: str):
+
+        if " " in senha:
+            print("Erro: Não deve conter espaço em senha.")
+            return False
+        elif not ((len(senha) > 5) and (len(senha) <= 64)):
+            print("Erro: senha deve conter, pelo menos, entre 6 à 64 caracteres")
+            return False
+        return True
+
+
     # Tela de Login
     def login(self):
 
@@ -25,14 +49,14 @@ class Cadastro_logic:
 
         while True:
             self.login_tela()
-            input_email = input("Usuario: ")
-            if input_email == variaveis_global.voltar_flair:
+            input_usuario = input("Usuario: ")
+            if input_usuario == variaveis_global.voltar_flair:
                 return -1
             input_senha = input("Senha: ")
             if input_senha == variaveis_global.voltar_flair:
                 return -1
 
-            index = self.usuario_auth(input_email, input_senha)
+            index = self.usuario_auth(input_usuario, input_senha)
             if index != -1:
                 return index
             else:
@@ -42,21 +66,27 @@ class Cadastro_logic:
     def cadastro(self):
 
         usuario = {}
-        cad_email = ""
+        cad_usuario = ""
         cad_senha = ""
 
         while True:
-            cad_email = input("Insira seu usuario: ")
-            if cad_email == variaveis_global.voltar_flair:
+            print()
+            cad_usuario = input("Insira seu usuario: ")
+            cad_usuario = cad_usuario.strip()
+            if cad_usuario == variaveis_global.voltar_flair:
                 return
-            elif not self.usuario_existe(cad_email):
-                print("Já existe um usuário.")
+            elif not self.nome_de_usuario_check(cad_usuario):
+                continue
+            elif self.usuario_existe(cad_usuario) != -1:
+                print("Já existe um usuário com esse nome de usuário.")
                 continue
 
             while True:
                 cad_senha = input("Insira a senha: ")
                 if cad_senha == variaveis_global.voltar_flair:
                     return
+                if not self.senha_check(cad_senha):
+                    continue
                 cad_senha_nov = input("Insira a senha novamente: ")
                 if cad_senha == variaveis_global.voltar_flair:
                     return
@@ -66,10 +96,8 @@ class Cadastro_logic:
                     print("As senhas não coincidem, tente novamente.")
             break
 
-        usuario["usuario"] = cad_email
+        usuario["usuario"] = cad_usuario
         usuario["senha"] = cad_senha
-
-
 
         db.usuarios.append(usuario.copy())
         usuario.clear()
@@ -77,16 +105,40 @@ class Cadastro_logic:
         print("CADASTRADO COM SUCESSO;")
         time.sleep(1.5)
 
-    def usuario_existe(self, email):
+    def usuario_existe(self, usr):
 
         for index, usuario in enumerate(db.usuarios):
-            if usuario["usuario"] == email:
+            if usuario["usuario"] == usr:
                 return index
         return -1
 
-    def usuario_auth(self, email, senha):
+    def usuario_auth(self, usr, senha):
 
         for index, usuario in enumerate(db.usuarios):
-            if usuario["usuario"] == email and usuario["usuario"] == senha:
+            if usuario["usuario"] == usr and usuario["senha"] == senha:
                 return index
         return -1
+
+    def editar_nome_de_usuario(self, index: int, new_usr_name: str):
+        try:
+            if not self.nome_de_usuario_check(new_usr_name):
+                return False
+            db.usuarios[index]["usuario"] = new_usr_name
+        except:
+            print("Ocorreu um problema ao editar o nome de usuário. Por favor, entre em contato com os administradores para reportar o Bug.")
+            return False
+        return True
+
+    def editar_senha_de_usuario(self, index: int, old_usr_pass: str, new_usr_pass: str):
+
+        if not old_usr_pass == db.usuarios[index]["senha"]:
+            print("Senha incorreta.")
+            return False
+        try:
+            if not self.senha_check(new_usr_pass):
+                return False
+            db.usuarios[index]["senha"] = new_usr_pass
+        except:
+            print("Ocorreu um problema ao editar a senha de usuário. Por favor, entre em contato com os administradores para reportar o Bug.")
+            return False
+        return True
